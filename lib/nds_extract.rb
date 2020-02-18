756df0aa6be4c4b91276a61104fa858e46e1a41d
Brett Bedevian <brettbedevian@gmail.com>
2020-02-18 17:37:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,16 +4,16 @@ require 'directors_database'
 def directors_totals(nds)
   new_hash = {}
  dir_index = 0
-  while dir_index < directors_database.length do 
+  while dir_index < nds.length do 
   dir_total= 0
-  movies = directors_database[dir_index][:movies]
+  movies = nds[dir_index][:movies]
   i = 0
   while i < movies.length do
     ww_gross = movies[i][:worldwide_gross]
     dir_total += ww_gross
     i+=1
   end
-  new_hash[:directors_database[dir]] = dir_total
+  new_hash[:nds[dir_index]] = dir_total
   dir +=1
 end
 new_hash
