686ec45cc2643372ca698348b1ae971dcc1d641b
Brett Bedevian <brettbedevian@gmail.com>
2020-02-18 16:36:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ director_total = 0
   ww_gross = directors_database[0][:worldwide_gross]
   index = 0
   while index < movies.length do
-    titles = ss_movies[index][:title]
+    dir_gross = ww_gross[index][:worldwide_gross]
     index +=1
 return director_total
   nil
