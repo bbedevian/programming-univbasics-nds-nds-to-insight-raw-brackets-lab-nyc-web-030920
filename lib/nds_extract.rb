ccb08ef4f5ce968acf6641c646fc2d5eb4d3d1a9
Brett Bedevian <brettbedevian@gmail.com>
2020-02-18 16:34:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,7 +3,12 @@ require 'directors_database'
 
 def directors_totals(nds)
 director_total = 0 
+  movies = directors_database[0][:movies]
+  index = 0
 
+  while index < ss_movies.length do
+    titles = ss_movies[index][:title]
+    index +=1
 return director_total
   nil
 end
